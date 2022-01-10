bdac204c0a84bf1660fa2a85cbca2af8334ea3b0
TitaniumTronic <89898497+TitaniumTronic@users.noreply.github.com>
2022-01-10 15:30:13 +0800

    I did an oopsie

--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='landing'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('forum/', views.forum, name='forum')
-]
