8cb56cd796a18ad15655dee8e3cef62a57b0d939
DrokoDomi <droko@gmx.de>
2022-01-11 22:43:15 +0100

    Added different setting files to organize local and production settings

---
+++ settings/local.py
@@ -0,0 +1,5 @@
+from settings.base import *
+
+SECRET_KEY = "localtestingkey"
+print("i don't get it")
+DEBUG = True
---
+++ settings/prod.py
@@ -0,0 +1,33 @@
+import os
+from settings.base import *
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+DEBUG = False
+
+
+ALLOWED_HOSTS = ["eduzec.herokuapp.com", "*"]
+
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd6778krnp5ooid',
+        'USER': 'yedofkcgxmxyxq',
+        'PASSWORD': '2aa6181fb4e9db9b4d321ea0b624e727003a2097c23edcdfd9a8c8228007cfad',
+        'HOST': 'ec2-54-247-96-153.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+STATIC_ROOT = "static/"
