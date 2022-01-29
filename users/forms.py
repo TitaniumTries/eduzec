4ab15c464d430143bf3dff0fbb068b00d4943b86
DrokoDomi <droko@gmx.de>
2022-01-29 21:07:45 +0100

    Removed unused import that caused Heroku and local problems (socket.fromshare is only available on Windows)

--- users/forms.py
+++ users/forms.py
@@ -1,17 +1,19 @@
-from socket import fromshare
-from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+
 from .models import CustomUser
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ("username", 'email', )
+        fields = ("username", 'email',)
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ("username", "email", "first_name", "last_name", "hide_email", )
+        fields = ("username", "email", "first_name", "last_name", "hide_email",)
+
 
 class CustomUserAuthenticationForm(AuthenticationForm):
     pass
