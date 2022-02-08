589355e6d2421659b6b3bc0c4db228a2c7232f59
TitaniumTronic <mbkpresident233@gmail.com>
2022-02-08 14:22:23 +0800

    testin some stuff (not finished at all lol)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.backends import ModelBackend
+from users.models import CustomUser
 
-# Create your models here.
+# This is the question model
+class Question(models.Model):
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    detail = models.TextField()
+    add_time = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+# This is the answer model
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    detail = models.TextField()
+    add_time = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.detail
+
+
+    
+    
