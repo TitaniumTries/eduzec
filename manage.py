bfad7319922cd0da422395e28175afa68ffb7a0e
Slavi <slavi.stoev97@gmail.com>
2022-04-18 09:30:23 +0300

    Fixed settings specifications. No more --settings=settings.local.

--- manage.py
+++ manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
+load_dotenv()
+ENV_ROLE = os.getenv('ENV_ROLE')
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eduzec.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.%s' % ENV_ROLE)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
